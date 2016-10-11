370ed8d0f310568ca2565362c920c67de85d8675
semerus <semerus4u@gmail.com>
2016-10-11 16:30:05 +0900

    minor changes to export pikachu data

--- ProjectPika/Assets/Scripts/PlayManager.cs
+++ ProjectPika/Assets/Scripts/PlayManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class PlayManager : MonoBehaviour {
-	private PlayManager instance = null; //for singleton design
+	private static PlayManager instance = null; //for singleton design
     public GameManager gameManager;
     public Pikachu pikachu;
 
