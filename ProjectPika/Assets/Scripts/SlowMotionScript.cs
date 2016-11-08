662c908b47870b91687c6414ff3652621d5413ba
송수찬\SongSoochan <s1928a1tommy@gmail.com>
2016-11-08 11:29:57 +0900

    슬로우모션 이펙트 추가.

---
+++ ProjectPika/Assets/Scripts/SlowMotionScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowMotionScript : MonoBehaviour {
+
+    private float slowTimeScale;
+    private float factor = 4f;
+
+	// Use this for initialization
+	void Start () {
+        slowTimeScale = Time.timeScale / factor;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.P))
+            slowMotion();
+	}
+
+    void slowMotion()
+    {
+        Time.timeScale = slowTimeScale;
+        Time.fixedDeltaTime = Time.fixedDeltaTime * Time.timeScale;
+    }
+}
